a2c175ebffe0c24e01e877b78b07c85addf27baf
Derek Arrotta <derek.arrotta@gmail.com>
2020-11-24 13:30:47 -0500

    add basix render to cartList for now

---
+++ src/cartList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class cartList extends Component {
+  render() {
+    return (
+      <div>
+        Derek
+      </div>
+    )
+  }
+}
+
+export default cartList;
