8c0e575b88ad0108a8322513b86f238ae4ae2c78
Derek Arrotta <derek.arrotta@gmail.com>
2020-11-27 12:20:24 -0500

    add in currency formatter component

--- src/Features.js
+++ src/Features.js
@@ -1,26 +1,21 @@
 import React, { Component } from 'react';
 import slugify from 'slugify';
+import CurrencyFormatter from './CurrencyFormatter';
 
-const USCurrencyFormat = new Intl.NumberFormat('en-US', {
-  style: 'currency',
-  currency: 'USD',
-});
-
+//const USCurrencyFormat = new Intl.NumberFormat('en-US', {
+//  style: 'currency',
+//  currency: 'USD',
+//});
+//({USCurrencyFormat.format(item.cost)})
 
 class Features extends Component {
 
   render() {
-    //
-    //console.log(this.props.features);
     const features = Object.keys(this.props.features).map((feature, idx) => {
       const featureHash = feature + '-' + idx;
       const options = this.props.features[feature].map((item) => {
         const itemHash = slugify(JSON.stringify(item));
-        //console.log(feature);
-        //console.log(this.props.selected);
-        console.log(this.props.selected[feature].name);
-        //console.log(item.name);
-        //console.log(item);
+        console.log(this.props.selected[feature]);
         return (
           <div key={itemHash} className="feature__item">
             <input
@@ -31,7 +26,7 @@ class Features extends Component {
               checked={item.name === this.props.selected[feature].name}
               onChange={e => this.props.updateFeature(feature, item)} />
             <label htmlFor={itemHash} className="feature__label">
-              {item.name} ({USCurrencyFormat.format(item.cost)})
+              {item.name} <CurrencyFormatter amount={item.cost}/>
             </label>
           </div>
         );
